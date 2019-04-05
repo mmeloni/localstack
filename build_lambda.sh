1416607eac53bc039695da03504e096f0e78438c
Michele Meloni <cleaversdev@gmail.com>
2019-04-05 09:30:53 +0200

    Add flag to build statically linked binary go lambda

--- build_lambda.sh
+++ build_lambda.sh
@@ -3,16 +3,17 @@
 LAMBDADIR="/opt/code/localstack/go/src/gitlab.easy-network.it/Payfolder/loan"
 
 if [ ! -d "$LAMBDADIR" ]; then
-    apk add zip go git bash gcc musl-dev openssl
+    echo @newest http://dl-cdn.alpinelinux.org/alpine/v3.9/community >> /etc/apk/repositories
+    apk add zip go@newest git bash gcc musl-dev openssl
 
-    mkdir -p go/src/gitlab.easy-network.it/Payfolder/loan go/bin
+    mkdir -p /opt/code/localstack/go/src/gitlab.easy-network.it/Payfolder/loan go/bin
 
     export GOPATH=/opt/code/localstack/go
     export PATH=${GOPATH}/bin:$PATH
     export GOBIN=/opt/code/localstack/go/bin
     export PATH=$PATH:/opt/code/localstack/go/bin
     echo "Cloning gitlab.easy-network.it/Payfolder/loan.git..."
-    git clone https://gitlab+deploy-token-1:2MMYxEmQBmm_VhVAyiuh@gitlab.easy-network.it/Payfolder/loan.git go/src/gitlab.easy-network.it/Payfolder/loan/
+    git clone https://gitlab+deploy-token-1:2MMYxEmQBmm_VhVAyiuh@gitlab.easy-network.it/Payfolder/loan.git /opt/code/localstack/go/src/gitlab.easy-network.it/Payfolder/loan/
 
     echo "Dep install..."
     curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
@@ -21,9 +22,10 @@ if [ ! -d "$LAMBDADIR" ]; then
     echo "Install dependencies..."
     dep ensure
     echo "Building..."
-    go build -o main
+    go build -v -a -ldflags "-linkmode external -extldflags -static" -o main
+    echo "Zipping..."
     zip main.zip main assets/*
-
+    echo "Install terraform..."
     wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
     unzip terraform_0.11.13_linux_amd64.zip
     mv terraform /usr/local/bin/
